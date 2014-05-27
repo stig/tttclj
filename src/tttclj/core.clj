e3ec96c3777658d568a16af4b0319443cbf22538
Stig Brautaset <stig@brautaset.org>
2014-05-27 23:56:43 +0100

    Add `opponent' and `make-move' functions (& tests)

--- src/tttclj/core.clj
+++ src/tttclj/core.clj
@@ -2,9 +2,19 @@
 
 (defn create-game []
   {:player :x
-   :tiles (repeat 9 :-) } )
+   :tiles (apply vector (repeat 9 :-)) } )
 
- (defn possible-moves [game]
+(defn opponent [player]
+  (cond (= :x player) :o
+        (= :o player) :x))
+
+(defn make-move [idx game]
+  (let [p (:player game)
+        t (:tiles game)
+        g (assoc game :player (opponent p))]
+    (assoc-in g [:tiles idx] p)))
+
+(defn possible-moves [game]
   (->> (map-indexed vector (:tiles game))
        (keep #(cond (= :- (second %)) (first %)))))
 
--- test/tttclj/t_core.clj
+++ test/tttclj/t_core.clj
@@ -10,6 +10,17 @@
        (fact "no tiles are set to start with"
              (frequencies (:tiles (create-game))) => { :- 9 }))
 
+(facts "about `opponent'"
+       (opponent :x) => :o
+       (opponent :o) => :x)
+
+(facts "about `make-move'"
+       (fact "the player should swap to :o"
+             (:player (make-move 3 (create-game))) => :o)
+       (fact "one tile is taken now"
+             (frequencies (:tiles (make-move 3 (create-game)))) => { :- 8 :x 1}))
+
+
 (facts "about `possible-moves'"
        (fact "there should be 9 to start"
              (count (possible-moves (create-game))) => 9))
