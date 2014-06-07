c7a2628c2e7e9e75e7b86d32ac7ac361f98f4ea8
Stig Brautaset <stig@brautaset.org>
2014-06-08 00:37:54 +0100

    Refactor minimax function

--- src/tttclj/solver.clj
+++ src/tttclj/solver.clj
@@ -13,15 +13,8 @@
           (recur (rest moves) (if (> sc max_sc) sc max_sc)))
         max_sc))))
 
-(defn minimax-middle [game]
-  (if (is-game-over? game)
-    {}
-    (->> (possible-moves game)
-         (map (fn [m]
-                [m (minimax-inner (successor game m))]))
-         (into {}))))
-
 (defn minimax [game]
-  (if-let [scores (minimax-middle game)]
-    (key (apply max-key val scores))
-    nil))
+  (key (apply max-key val 
+              (->> (possible-moves game)
+                   (map (fn [m] [m (minimax-inner (successor game m))]))
+                   (into {})))))
