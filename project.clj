33243661095d88d5b54d7ff839b640dfc5988f0c
Stig Brautaset <stig@brautaset.org>
2014-06-08 20:55:25 +0100

    Adding resources directory

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject tttclj "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [http-kit "2.1.16"]
+                 [compojure "1.1.8"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+  :main tttclj.web)
   
