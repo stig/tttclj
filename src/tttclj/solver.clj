0bae6ac7e244d921549e69b7821bf6d89ae04661
Stig Brautaset <stig@brautaset.org>
2014-06-08 01:39:40 +0100

    Made the minimax function take a depth & added more tests

--- src/tttclj/solver.clj
+++ src/tttclj/solver.clj
@@ -1,15 +1,16 @@
 (ns tttclj.solver
   (:use tttclj.core))
 
-(defn minimax-inner [game]
-  (if (is-game-over? game)
+(defn minimax-inner [game depth]
+  (if (or (zero? depth)
+          (is-game-over? game))
     (fitness game)
     (->> (possible-moves game)
-         (map (fn [move] (- (minimax-inner (successor game move)))))
+         (map (fn [move] (- (minimax-inner (successor game move) (dec depth)))))
          (apply max))))
 
-(defn minimax [game]
+(defn minimax [game depth]
   (->> (possible-moves game)
-       (map (fn [move] [move (- (minimax-inner (successor game move)))]))
+       (map (fn [move] [move (- (minimax-inner (successor game move) (dec depth)))]))
        (apply max-key second)
        (first)))
--- test/tttclj/t_solver.clj
+++ test/tttclj/t_solver.clj
@@ -4,7 +4,24 @@
   (:use [tttclj.solver]))
 
 (facts "about minimax"
-       (fact "it picks the center @ xo- --- ---"
+       (fact "it picks the center @ xo- --- --- at ply 9"
              (minimax (-> (create-game)
                           (successor 0)
-                          (successor 1))) => 4))
+                          (successor 1)) 9) => 4)
+       (fact "it picks the center @ blank board at ply 1"
+             (minimax (create-game) 1) => 4)
+       (fact "it plays a predictable game @ depth 3"
+             (loop [game (create-game) history []]
+               (if (is-game-over? game)
+                 (conj history game)
+                 (recur (successor game (minimax game 3)) (conj history game))))
+             => [{:player :x, :tiles [:- :- :- :- :- :- :- :- :-]}
+                 {:player :o, :tiles [:- :- :- :- :x :- :- :- :-]}
+                 {:player :x, :tiles [:- :- :- :- :x :- :- :- :o]}
+                 {:player :o, :tiles [:- :- :- :x :x :- :- :- :o]}
+                 {:player :x, :tiles [:- :- :- :x :x :o :- :- :o]}
+                 {:player :o, :tiles [:- :- :x :x :x :o :- :- :o]}
+                 {:player :x, :tiles [:- :- :x :x :x :o :o :- :o]}
+                 {:player :o, :tiles [:- :- :x :x :x :o :o :x :o]}
+                 {:player :x, :tiles [:- :o :x :x :x :o :o :x :o]}
+                 {:player :o, :tiles [:x :o :x :x :x :o :o :x :o]}]))
