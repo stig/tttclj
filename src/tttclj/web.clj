c790af1c335595c9af75cd5e5e82b5ca77200d48
Stig Brautaset <stig@brautaset.org>
2014-06-23 09:59:53 +0100

    Revert "Autoload!"
    This reverts commit 596b29755abd637972d7a9f17835b2c61cb8fc5a.
    
    It causes 'lein midje' to fail.

--- src/tttclj/web.clj
+++ src/tttclj/web.clj
@@ -2,10 +2,8 @@
   (:require [chord.http-kit :refer [wrap-websocket-handler]]
             [clojure.core.async :refer [<! >! put! close! go go-loop timeout]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.handler :refer [site]]
             [compojure.route :refer [resources]]
             [org.httpkit.server :refer [run-server]]
-            [ring.middleware.reload :refer [wrap-reload]]
             [ring.util.response :refer [redirect]]
             [tttclj.core :refer [create-game possible-moves successor game-over?]]
             [tttclj.prep :refer [prep]]
@@ -17,7 +15,9 @@
 (defn ws-handler [{:keys [ws-channel] :as req}]
   (println "Opened connection from" (:remote-addr req))
   (let [game (atom (create-game))]
-    (go (>! ws-channel (prep @game)))
+    ;; Let's send a new game straight away.
+    (go
+      (>! ws-channel (prep @game)))
     ;; Wait for moves from client. Apply them, and send new board back.
     (go-loop []
       (when-let [{:keys [message error] :as msg} (<! ws-channel)]
@@ -32,19 +32,13 @@
           (when-not (game-over? @game)
             (recur)))))))
 
-(defn in-dev? [args]
-  true) ;; TODO find a better way to do this...
-
-(defroutes all-routes
+(defroutes app
   (resources "/")
   (GET "/ws" [] (-> ws-handler
                     (wrap-websocket-handler {:format :edn})))
   (GET "/" [] (redirect "/index.html")))
 
 (defn -main [& args]
-  (let [port (if (empty? args) 8080 (Integer. (first args)))
-        handler (if (in-dev? args)
-                  (wrap-reload (site #'all-routes))
-                  (site all-routes))]
-    (run-server handler {:port port :join? false})
+  (let [port (if (empty? args) 8080 (Integer. (first args)))]
+    (run-server app {:port port :join? false})
     (println (str "Started server on http://localhost:" port))))
