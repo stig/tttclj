7237231271f6a58b2bb29d7c402c2daef3242c9e
Stig Brautaset <stig@brautaset.org>
2014-06-24 22:19:47 +0100

    Added test for envelope unwrapper

--- src/tttclj/web.clj
+++ src/tttclj/web.clj
@@ -1,12 +1,20 @@
 (ns tttclj.web
   (:require [chord.http-kit :refer [wrap-websocket-handler]]
-            [clojure.core.async :refer [>! <! go chan]]
+            [clojure.core.async :refer [>! <! go go-loop chan]]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [resources]]
             [org.httpkit.server :refer [run-server]]
             [ring.util.response :refer [redirect]]
             [tttclj.core :refer [create-game]]
             [tttclj.game-loop :refer [start-game-loop]]))
+
+(defn envelope-unwrapper [ws-chan]
+  (let [proxy (chan)]
+    (go-loop []
+      (when-let [msg (<! ws-chan)]
+        (>! proxy (:message msg))
+        (recur)))
+    proxy))
 
 (defn ws-handler [{:keys [ws-channel] :as req}]
   (println "Opened connection from" (:remote-addr req))
--- test/tttclj/web_test.clj
+++ test/tttclj/web_test.clj
@@ -1,4 +1,13 @@
 (ns tttclj.web-test
-  (:require [tttclj.web :refer :all]
-            [midje.sweet :refer :all]))
+  (:require [clojure.core.async :refer [>! <! <!! go chan]]
+            [midje.sweet :refer :all]
+            [tttclj.web :refer :all]))
 
+(fact "it unwraps a message"
+      (let [ws-chan (chan)
+            proxy (envelope-unwrapper ws-chan)]
+        (go
+          (>! ws-chan {:message "lol"})
+          (>! ws-chan {:message "lol!!!"}))
+        (<!! proxy) => "lol"
+        (<!! proxy) => "lol!!!"))
